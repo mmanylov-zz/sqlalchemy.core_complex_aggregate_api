690f6bd6ff03210c54d57d80ebb169336fe09087
Maksim Manylov <maximalist888@icloud.com>
2021-05-18 16:21:21 +0300

    added sorting

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask import request
 from sqlalchemy import func
+import json
 
 
 app = Flask(__name__)
@@ -55,6 +56,11 @@ METRIC_ATTR_MAPPING = {
     'channel': Metric.channel,
     'country': Metric.country,
     'os': Metric.os,
+    'impressions': Metric.impressions,
+    'clicks': Metric.clicks,
+    'installs': Metric.installs,
+    'spend': Metric.spend,
+    'revenue': Metric.revenue,
 }
 
 
@@ -64,10 +70,33 @@ def metrics_get():
     filter_dict = req_json.get('filter')
     group_by_list = req_json.get('group_by')
     if group_by_list:
-        attr_list = [v for k, v in METRIC_ATTR_MAPPING.items() if k in group_by_list]
-        q = db.session.query(func.count(Metric.id), func.sum(Metric.impressions), func.sum(Metric.clicks),
-                             func.sum(Metric.installs), func.sum(Metric.spend), func.sum(Metric.revenue))\
-            .group_by(*attr_list)
+        attr_dict_to_group_by = {k:v for k, v in METRIC_ATTR_MAPPING.items() if k in group_by_list}
+        header_row = ['count(metric.id)', 'sum(metric.impressions)', 'sum(metric.clicks)', 'sum(metric.installs)',
+                      'sum(metric.spend)', 'sum(metric.revenue)']
+        header_row.extend(attr_dict_to_group_by.keys())
+        metric_id_count = func.count(Metric.id)
+        metric_impressions_sum = func.sum(Metric.impressions)
+        metric_clicks_sum = func.sum(Metric.clicks)
+        metric_installs_sum = func.sum(Metric.installs)
+        metric_spend_sum = func.sum(Metric.spend)
+        metric_revenue_sum = func.sum(Metric.revenue)
+        group_by_order_mapping = {
+            'id': metric_id_count,
+            'impressions': metric_impressions_sum,
+            'clicks': metric_clicks_sum,
+            'installs': metric_installs_sum,
+            'spend': metric_spend_sum,
+            'revenue': metric_revenue_sum,
+            'date': Metric.date,
+            'channel': Metric.channel,
+            'country': Metric.country,
+            'os': Metric.os
+        }
+        aggregates_list = [metric_id_count, metric_impressions_sum, metric_clicks_sum, metric_installs_sum,
+                           metric_spend_sum, metric_revenue_sum]
+        aggregates_list.extend(attr_dict_to_group_by.values())
+        q = db.session.query(*aggregates_list)\
+            .group_by(*attr_dict_to_group_by.values())
     else:
         q = db.session.query(Metric)
 
@@ -83,11 +112,36 @@ def metrics_get():
         if filter_dict.get('os'):
             q = q.filter(Metric.os.in_(filter_dict['os']))
 
+    sort_dict = req_json.get('sort')
+    if sort_dict:
+        if group_by_list:
+            sort_list = []
+            for attr, order in sort_dict.items():
+                if order == 'ASC':
+                    sort_list.append(group_by_order_mapping[attr].asc())
+                else:
+                    sort_list.append(group_by_order_mapping[attr].desc())
+            if sort_list:
+                q = q.order_by(*sort_list)
+        else:
+            sort_list = []
+            for attr, order in sort_dict.items():
+                if order == 'ASC':
+                    sort_list.append(METRIC_ATTR_MAPPING[attr].asc())
+                else:
+                    sort_list.append(METRIC_ATTR_MAPPING[attr].desc())
+            if sort_list:
+                q = q.order_by(*sort_list)
 
-    # metrics = q.all()
-    # metrics_dicts = [m.to_dict() for m in metrics]
-    # return json.dumps(metrics_dicts)
-    return f'Rows selected: {str(q.count())}\nTotal rows: {str(db.session.query(Metric).count())}'
+    metrics = q.all()
+
+    if group_by_list:
+        result_list_of_dicts = []
+        for m in metrics:
+            result_list_of_dicts.append(dict(zip(header_row, m)))
+        return json.dumps(result_list_of_dicts)
+    else:
+        return json.dumps([m.to_dict() for m in metrics])
 
 
 if __name__ == '__main__':
